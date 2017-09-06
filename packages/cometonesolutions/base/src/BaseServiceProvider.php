994c0e14de8b4e11cfc8c8ad062d05d77d52ae30
Howell Martinez <howellmartinez@gmail.com>
2017-09-06 17:34:44 +0800

    no message

---
+++ packages/cometonesolutions/base/src/BaseServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace cometonesolutions\base;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
---
+++ packages/cometonesolutions/base/src/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmartinez
+ * Date: 06/09/2017
+ * Time: 1:37 PM
+ */
